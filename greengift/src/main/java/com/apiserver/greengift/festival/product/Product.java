b000fafd793455a82a2f01088000b227fc97b585
WoeunJung <leena09123@naver.com>
2023-12-02 13:35:45 +0900

    feat: add Product Entity, Repository

---
+++ greengift/src/main/java/com/apiserver/greengift/festival/product/Product.java
@@ -0,0 +1,52 @@
+package com.apiserver.greengift.festival.product;
+
+import com.apiserver.greengift.festival.Festival;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "product_tb")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Festival festival;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "due_date", nullable = false)
+    private LocalDate dueDate;
+
+    @Column(name = "extra_amount", nullable = false)
+    private Long extraAmount;
+
+    @Lob
+    private String image;
+
+    @Column(nullable = false)
+    private String company;
+
+    @Column(nullable = false)
+    private Long rank;
+
+    @Builder
+    public Product(Long id, Festival festival, String name, LocalDate dueDate,
+                   Long extraAmount, String image, String company, Long rank){
+        this.id = id;
+        this.festival = festival;
+        this.name = name;
+        this.dueDate = dueDate;
+        this.extraAmount = extraAmount;
+        this.image = image;
+        this.company = company;
+        this.rank = rank;
+    }
+}
