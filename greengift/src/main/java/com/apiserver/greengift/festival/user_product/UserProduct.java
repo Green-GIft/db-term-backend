8c1973824755bc9dd1247c2872270ac991240861
WoeunJung <leena09123@naver.com>
2023-12-02 13:36:40 +0900

    feat: add UserProduct Entity, Repository

---
+++ greengift/src/main/java/com/apiserver/greengift/festival/user_product/UserProduct.java
@@ -0,0 +1,38 @@
+package com.apiserver.greengift.festival.user_product;
+
+import com.apiserver.greengift.festival.constant.ProductCategory;
+import com.apiserver.greengift.festival.product.Product;
+import com.apiserver.greengift.festival.user_festival.UserFestival;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user_product_tb")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private UserFestival userFestival;
+
+    @Column(name = "product_category", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ProductCategory category;
+
+    @Builder
+    public UserProduct(Long id, Product product, UserFestival userFestival, ProductCategory category){
+        this.id = id;
+        this.product = product;
+        this.userFestival = userFestival;
+        this.category = category;
+    }
+
+}
+
